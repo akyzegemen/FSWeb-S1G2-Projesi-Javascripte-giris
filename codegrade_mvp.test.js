4485bd3864d62dd085842df10643b8e12f00b402
akyuzegemen <akyuzegemen@sabanciuniv.edu>
2024-11-03 16:25:42 +0300

    gorevler tamamlandı

--- codegrade_mvp.test.js
+++ codegrade_mvp.test.js
@@ -1,7 +1,7 @@
 import functions from './index';
 
 describe('saFunction', ()=>{
-    it('sa as döner', ()=>{
+    it('sa as doner', ()=>{
         expect(functions.sa()).toBe('as');
     })
 });
@@ -13,85 +13,85 @@ describe('carpmaFunction', ()=>{
 });
 
 describe('kopeginYasiFunction', ()=>{
-    it('sonuç insanYasi * 7', ()=>{
+    it('sonuc insanYasi * 7', ()=>{
         expect(functions.kopeginYasi(5)).toBe(35);
     })
 });
 
 describe('oyunFunction', ()=>{
-    it('sonuç Kazandın!, Kaybettin! veya Beraberlik', ()=>{
-        expect(functions.oyun('Taş', 'Makas')).toBe('Kazandın!');
+    it('sonuc Kazandin!, Kaybettin! veya Beraberlik', ()=>{
+        expect(functions.oyun('Tas', 'Makas')).toBe('Kazandin!');
     })
-    it('sonuç Kazandın!, Kaybettin! veya Beraberlik', ()=>{
-        expect(functions.oyun('Taş', 'Kağıt')).toBe('Kaybettin!');
+    it('sonuc Kazandin!, Kaybettin! veya Beraberlik', ()=>{
+        expect(functions.oyun('Tas', 'Kagit')).toBe('Kaybettin!');
     })
-    it('sonuç Kazandın!, Kaybettin! veya Beraberlik', ()=>{
-        expect(functions.oyun('Taş', 'Taş')).toBe(`Beraberlik`);
+    it('sonuc Kazandin!, Kaybettin! veya Beraberlik', ()=>{
+        expect(functions.oyun('Tas', 'Tas')).toBe(`Beraberlik`);
     })
-    it('sonuç Kazandın!, Kaybettin! veya Beraberlik', ()=>{
-        expect(functions.oyun('Kağıt', 'Taş')).toBe(`Kazandın!`);
+    it('sonuc Kazandin!, Kaybettin! veya Beraberlik', ()=>{
+        expect(functions.oyun('Kagit', 'Tas')).toBe(`Kazandin!`);
     })
-    it('sonuç Kazandın!, Kaybettin! veya Beraberlik', ()=>{
-        expect(functions.oyun('Kağıt', 'Makas')).toBe(`Kaybettin!`);
+    it('sonuc Kazandin!, Kaybettin! veya Beraberlik', ()=>{
+        expect(functions.oyun('Kagit', 'Makas')).toBe(`Kaybettin!`);
     })
-    it('sonuç Kazandın!, Kaybettin! veya Beraberlik', ()=>{
-        expect(functions.oyun('Kağıt', 'Kağıt')).toBe(`Beraberlik`);
+    it('sonuc Kazandin!, Kaybettin! veya Beraberlik', ()=>{
+        expect(functions.oyun('Kagit', 'Kagit')).toBe(`Beraberlik`);
     })
-    it('sonuç Kazandın!, Kaybettin! veya Beraberlik', ()=>{
-        expect(functions.oyun('Makas', 'Kağıt')).toBe(`Kazandın!`);
+    it('sonuc Kazandin!, Kaybettin! veya Beraberlik', ()=>{
+        expect(functions.oyun('Makas', 'Kagit')).toBe(`Kazandin!`);
     })
-    it('sonuç Kazandın!, Kaybettin! veya Beraberlik', ()=>{
-        expect(functions.oyun('Makas', 'Taş')).toBe(`Kaybettin!`);
+    it('sonuc Kazandin!, Kaybettin! veya Beraberlik', ()=>{
+        expect(functions.oyun('Makas', 'Tas')).toBe(`Kaybettin!`);
     })
 });
 
 describe('milDonusturucuFunction', ()=>{
-    it('sonuç km * 0.621371', ()=>{
+    it('sonuc km * 0.621371', ()=>{
         expect(functions.milDonusturucu(10)).toBe(6.21371);
     })
 });
 
 describe('feetDonusturucuFunction', ()=>{
-    it('sonuç cm / 30.48', ()=>{
+    it('sonuc cm / 30.48', ()=>{
         expect(functions.feetDonusturucu(160)).toBe(5.2493438320209975);
     })
 });
 
 describe('cocukSarkisiFunction', ()=>{
-    it('girilen değere göre geri sayım yapan bir string döner', ()=>{
-        expect(functions.cocukSarkisi(5)).toBe(`${5} küçük maymun yatakta zıplamış, biri düşüp başını çarpmış, Anne doktoru aramış, Doktor çok kızmış: Bir daha yatakta zıplamak yok!`);
+    it('girilen degere gore geri sayim yapan bir string doner', ()=>{
+        expect(functions.cocukSarkisi(5)).toBe(`${5} kucuk maymun yatakta ziplamis, biri dusup basini carpmis, Anne doktoru aramis, Doktor cok kizmis: Bir daha yatakta ziplamak yok!`);
     })
 });
 
 describe('notHesaplaFunction', ()=>{
-    it('A olmalıydı', ()=>{
-        expect(functions.notHesapla(90)).toBe('A aldın');
+    it('A olmaliydi', ()=>{
+        expect(functions.notHesapla(90)).toBe('A aldin');
     })
-    it('A olmalıydı', ()=>{
-      expect(functions.notHesapla(95)).toBe('A aldın');
+    it('A olmaliydi', ()=>{
+      expect(functions.notHesapla(95)).toBe('A aldin');
   })
-    it('B olmalıydı', ()=>{
-        expect(functions.notHesapla(80)).toBe('B aldın');
+    it('B olmaliydi', ()=>{
+        expect(functions.notHesapla(80)).toBe('B aldin');
     })
-    it('B olmalıydı', ()=>{
-      expect(functions.notHesapla(85)).toBe('B aldın');
+    it('B olmaliydi', ()=>{
+      expect(functions.notHesapla(85)).toBe('B aldin');
   })
-    it('C olmalıydı', ()=>{
-        expect(functions.notHesapla(70)).toBe('C aldın');
+    it('C olmaliydi', ()=>{
+        expect(functions.notHesapla(70)).toBe('C aldin');
     })
-    it('C olmalıydı', ()=>{
-      expect(functions.notHesapla(75)).toBe('C aldın');
+    it('C olmaliydi', ()=>{
+      expect(functions.notHesapla(75)).toBe('C aldin');
   })
-    it('D olmalıydı', ()=>{
-        expect(functions.notHesapla(60)).toBe('D aldın');
+    it('D olmaliydi', ()=>{
+        expect(functions.notHesapla(60)).toBe('D aldin');
     })
-    it('D olmalıydı', ()=>{
-      expect(functions.notHesapla(65)).toBe('D aldın');
+    it('D olmaliydi', ()=>{
+      expect(functions.notHesapla(65)).toBe('D aldin');
   })
-    it('F olmalıydı', ()=>{
-        expect(functions.notHesapla(59)).toBe('F aldın');
+    it('F olmaliydi', ()=>{
+        expect(functions.notHesapla(59)).toBe('F aldin');
     })
-    it('F olmalıydı', ()=>{
-      expect(functions.notHesapla(20)).toBe('F aldın');
+    it('F olmaliydi', ()=>{
+      expect(functions.notHesapla(20)).toBe('F aldin');
   })
 });
--- index.js
+++ index.js
@@ -1,180 +1,270 @@
-/* Görev 1: Isınalım Biraz :) */
+/* Gorev 1: Isinalim Biraz :) */
 
-/* TÜM CEVAPLARINI BİR FUNCTION İÇİNDE VERDİĞİNİZ EMİN OL (GÖREV 1D - GÖREV 6). AKSİ TAKTİRDE, OTOMATİK TEST SİSTEMİ ÇALIŞMAYACAKTIR. */
+/* TuM CEVAPLARINI BİR FUNCTION İcİNDE VERDİgİNİZ EMİN OL (GoREV 1D - GoREV 6). AKSİ TAKTİRDE, OTOMATİK TEST SİSTEMİ cALIsMAYACAKTIR. */
 
-// Görev 1a, 1b ve 1c puanlamaya dahil değildir
+// Gorev 1a, 1b ve 1c puanlamaya dahil degildir
 
 /*
-Bu görevleri yaparken, yazdığın kod'un doğru çıktı verdiğini kontrol etmek için console.log'u kullanmanı öneririz.
-Bunun için cevabı direk console'a logla veya çağırdığın fonksiyondan dönen değeri console'a logla.  
-Örneğin:  console.log(ornekFonksiyon(deger1,deger2))
+Bu gorevleri yaparken, yazdigin kod'un dogru cikti verdigini kontrol etmek icin console.log'u kullanmani oneririz.
+Bunun icin cevabi direk console'a logla veya cagirdigin fonksiyondan donen degeri console'a logla.  
+ornegin:  console.log(ornekFonksiyon(deger1,deger2))
 */
 
 /*
-Görev 1a - Ehliyet Yaşı (puanlamaya dahil değildir)
+Gorev 1a - Ehliyet Yasi (puanlamaya dahil degildir)
 
-Aşağıdakileri yap:   
-   1. surucuYasi isimli bir değişken yaz ve bir sayı ata.
-   2. Eğer yaş 18'den büyük ise Console'da true, değilse false değeri yazdırın.
+Asagidakileri yap:   
+   1. surucuYasi isimli bir degisken yaz ve bir sayi ata.
+   2. Eger yas 18'den buyuk ise Console'da true, degilse false degeri yazdirin.
 
-   İPUCU: fonksiyon oluşturmaya gerek yok
+   İPUCU: fonksiyon olusturmaya gerek yok
 */
 
 /*
-Görev 1b - Değerler (puanlamaya dahil değildir)
+Gorev 1b - Degerler (puanlamaya dahil degildir)
 
-Aşağıdakileri yap:   
-   1. 2 değişken tanımla ve bunlara değerler ata (isimlendirme olarak birinciDeger ikinciDegeri kullanabilirsin)
-   2. 1. değeri 2. değer ile karşılaştıran bir koşul oluşturun
-   3. 2. adımdaki koşul doğru olduğunda 1. değeri değiştirin
-   4. 1. değeri Console'a yazdırın
+Asagidakileri yap:   
+   1. 2 degisken tanimla ve bunlara degerler ata (isimlendirme olarak birinciDeger ikinciDegeri kullanabilirsin)
+   2. 1. degeri 2. deger ile karsilastiran bir kosul olusturun
+   3. 2. adimdaki kosul dogru oldugunda 1. degeri degistirin
+   4. 1. degeri Console'a yazdirin
 
-   İPUCU: fonksiyon oluşturmaya gerek yok
+   İPUCU: fonksiyon olusturmaya gerek yok
 */
 
 /*
-Görev 1c - String bir değeri Number'a dönüştürün (puanlamaya dahil değildir)
+Gorev 1c - String bir degeri Number'a donusturun (puanlamaya dahil degildir)
 
-Aşağıdakileri yap:   
-   1. String tipinde ve değeri "1999" olan bir değişken oluşturun
-   2. Değeri "1999" olan bu String'i değeri 1999 olan bir integer'a dönüştürün
-   3. Sonucu Console'a yazdırın
+Asagidakileri yap:   
+   1. String tipinde ve degeri "1999" olan bir degisken olusturun
+   2. Degeri "1999" olan bu String'i degeri 1999 olan bir integer'a donusturun
+   3. Sonucu Console'a yazdirin
 
    İPUCU: Number metoduna bakabilirsin
 */
 
 /*
-Görev 1d - Çarpma
+Gorev 1d - carpma
  
-Aşağıdakileri yaparak carpma isimli fonksiyonu tamamla:   
-   1. 2 argüman alsın(a ve b olarak tanımlayabilirsin)
-   2. a ve b'yi çarpıp, sonucu dönsün
-   3. console.log(carpma(7,4)) ile yazdığın fonsiyonu test edin. Console'da sonucu 28 olarak görmelisin.
+Asagidakileri yaparak carpma isimli fonksiyonu tamamla:   
+   1. 2 arguman alsin(a ve b olarak tanimlayabilirsin)
+   2. a ve b'yi carpip, sonucu donsun
+   3. console.log(carpma(7,4)) ile yazdigin fonsiyonu test edin. Console'da sonucu 28 olarak gormelisin.
 */
 
-function carpma(/*buraya kodunu yazabilirsin*/) {
+function carpma(sayi1, sayi2) {
   /*buraya kodunu yazabilirsin*/
+  return sayi1 * sayi2;
+
 }
 
-/* Görev 2 : Köpeğin Yaşı */
+/* Gorev 2 : Kopegin Yasi */
 
 /*
-Aşağıdakileri yap:
- 1. Aşağıdaki kopeginYasi fonsiyonunu bir argüman alacak şekilde tanımla.
- 2. Gelen değeri kullanarak köpeğin yaşını hesapla (insanlarda 1 yıl köpeklerde 7 yıla denk gelir)
- 3. Hesaplanan köpeğin yaşını dönün.
+Asagidakileri yap:
+ 1. Asagidaki kopeginYasi fonsiyonunu bir arguman alacak sekilde tanimla.
+ 2. Gelen degeri kullanarak kopegin yasini hesapla (insanlarda 1 yil kopeklerde 7 yila denk gelir)
+ 3. Hesaplanan kopegin yasini donun.
  */
 
-function kopeginYasi(/*buraya kodunu yazabilirsin*/) {
-  /*buraya kodunu yazabilirsin*/
+function kopeginYasi(yas) {
+  return 7*yas;
+
 }
 
-/* Görev 3 */
+/* Gorev 3 */
 /*
-Aşağıdakileri oyun isimli fonksiyonu kullanarak yap.
-1. 2 argüman alın: oyuncunun tercihini alan bir string değer: "Taş", "Kağıt" veya "Makas" 
-   ve bilgisayarınkini alan: "Taş", "Kağıt" veya "Makas"
-   Not: string değerlerin yazımına dikkat! İlk harf büyük ve türkçe karakterler kullanarak. Yoksa testi geçemez.
-2. Aşağıdaki oyun kurallarına göre oyuncunun kazanıp kazanamadığını veya berabere kalıp kalmadığını dönün - (uygulamanın oluşturduğu çıktı tam olarak aşağıdakinin aynısı olmalı)
- - kazanma durumunda "Kazandın!" dönmeli
- - kaybetme durumunda "Kaybettin!" dönmeli
- - beraberlikte "Beraberlik" dönmeli
+Asagidakileri oyun isimli fonksiyonu kullanarak yap.
+1. 2 arguman alin: oyuncunun tercihini alan bir string deger: "Tas", "Kagit" veya "Makas" 
+   ve bilgisayarinkini alan: "Tas", "Kagit" veya "Makas"
+   Not: string degerlerin yazimina dikkat! İlk harf buyuk ve turkce karakterler kullanarak. Yoksa testi gecemez.
+2. Asagidaki oyun kurallarina gore oyuncunun kazanip kazanamadigini veya berabere kalip kalmadigini donun - (uygulamanin olusturdugu cikti tam olarak asagidakinin aynisi olmali)
+ - kazanma durumunda "Kazandin!" donmeli
+ - kaybetme durumunda "Kaybettin!" donmeli
+ - beraberlikte "Beraberlik" donmeli
 
-OYUNUN KURALLARI: Makas Kağıdı yener| Kağıt Taşı yener | Taş Makas'ı yener | veya beraberlik olur.
+OYUNUN KURALLARI: Makas Kagidi yener| Kagit Tasi yener | Tas Makas'i yener | veya beraberlik olur.
 */
 
 function oyun(oyuncu, bilgisayar) {
-  /*buraya kodunu yazabilirsin*/
+  if(oyuncu === "Tas")
+  {
+    if(bilgisayar === "Tas")
+    {
+      console.log("Beraberlik");
+    }
+    else if(bilgisayar === "Kagit")
+    {
+      console.log("Kaybettin!");
+    }
+    else
+    {
+      console.log("Kazandin!");
+    }
+  }
+
+  else if (oyuncu === "Kagit")
+  {
+    if(bilgisayar === "Tas")
+    {
+      console.log("Kazandin!");
+    }
+    else if(bilgisayar === "Kagit")
+    {
+      console.log("Beraberlik");
+    }
+    else
+    {
+      console.log("Kaybettin!");
+    }
+  }
+
+  else
+  {
+    if(bilgisayar === "Tas")
+      {
+        console.log("Kaybettin!");
+      }
+      else if(bilgisayar === "Kagit")
+      {
+        console.log("Kazandin!");
+      }
+      else
+      {
+        console.log("Beraberlik");
+      }
+  }
+
 }
 
-// Şimdi Taş, Kağıt, Makas oyununu bilgisayara karşı oynayalım!
+// simdi Tas, Kagit, Makas oyununu bilgisayara karsi oynayalim!
 /*
-Öncelikle aşağıdakileri yap:
-1. Bilgisayarın seçimini rastgele oluşturacağım bir fonksiyon tanımla. Örn: 
+oncelikle asagidakileri yap:
+1. Bilgisayarin secimini rastgele olusturacagim bir fonksiyon tanimla. orn: 
    function bilgisayarinSecimi() {
    
    }
-2. Fonsiyonun içinde bilgisayarın seçimi için bir değişken tanımla
-3. Math.random'ı kullanarak bilgisayarın seçimini oluşturun (Math.random 0-1 arasında bir değer verecek)
-4. Bu rastgele değeri "Taş", "Kağıt" veya "Makas"a dönüştüren bir koşul oluşturun
-5. Oluşan değeri geri dönün
+2. Fonsiyonun icinde bilgisayarin secimi icin bir degisken tanimla
+3. Math.random'i kullanarak bilgisayarin secimini olusturun (Math.random 0-1 arasinda bir deger verecek)
+4. Bu rastgele degeri "Tas", "Kagit" veya "Makas"a donusturen bir kosul olusturun
+5. Olusan degeri geri donun
 
-Şimdi kendi seçtiğin bir seçime karşı bilgisayarın rastgele oluşturduğu seçimi yukarıda yazdığın oyun fonsiyonu ile oynayın ve sonucu console'a yazdırın.
-Örn: console.log(oyun("Makas",bilgisayarinSecimi()))
+simdi kendi sectigin bir secime karsi bilgisayarin rastgele olusturdugu secimi yukarida yazdigin oyun fonsiyonu ile oynayin ve sonucu console'a yazdirin.
+orn: console.log(oyun("Makas",bilgisayarinSecimi()))
 */
+function bilgisayarinSecimi() 
+{
+  let num = Math.ceil(Math.random() * 3);
+  if(num === 1)
+  {
+    return "Kagit";
+  }
+  else if(num === 2)
+  {
+    return "Tas";
+  }
+  return "Makas";
 
-/* Görev 4 : Metrik Dönüştürücü */
+}
+console.log(oyun("Makas",bilgisayarinSecimi()))
+/* Gorev 4 : Metrik Donusturucu */
 
-//Görev 4a - Kilometreden Mil
+//Gorev 4a - Kilometreden Mil
 /*
-Aşağdaki milDonusturucu fonksiyonunu aşağıdakileri kullanarak tamamla:
-1. Kilometre değerini argüman olarak alın.
-2. Aldığın bu değeri Mil'e dönüştürün
-3. Mil değerini geri dönün
+Asagdaki milDonusturucu fonksiyonunu asagidakileri kullanarak tamamla:
+1. Kilometre degerini arguman olarak alin.
+2. Aldigin bu degeri Mil'e donusturun
+3. Mil degerini geri donun
 */
 
-function milDonusturucu(/*buraya kodunu yazabilirsin*/) {
-  /*buraya kodunu yazabilirsin*/
+function milDonusturucu(km) {
+  return km * (0.62137119);
 }
 
-//Görev 4b - Santimetreden Feet
+//Gorev 4b - Santimetreden Feet
 /*
-Aşağıdakileri feetDonusturucu fonsiyonunu kullanarak yap:
-1. Santimetre değerini argüman olarak alın.
-2. Aldığın bu değeri feet'e dönüştürün
-3. feet değerini geri dönün
+Asagidakileri feetDonusturucu fonsiyonunu kullanarak yap:
+1. Santimetre degerini arguman olarak alin.
+2. Aldigin bu degeri feet'e donusturun
+3. feet degerini geri donun
 
-Google'da arama ipucu: "feet cm dönüştürme"
+Google'da arama ipucu: "feet cm donusturme"
 */
 
-function feetDonusturucu(/*buraya kodunu yazabilirsin*/) {
-  /*buraya kodunu yazabilirsin*/
+function feetDonusturucu(cm) 
+{
+  let km = cm / 1000;
+  return milDonusturucu(km);  
 }
 
-/* Görev 5 : 5 küçük maymun yatakta zıplamış şarkısını çocuklar için hazırladığımı varsayalım. https://www.youtube.com/watch?v=e4EJ34xnlxk */
+/* Gorev 5 : 5 kucuk maymun yatakta ziplamis sarkisini cocuklar icin hazirladigimi varsayalim. https://www.youtube.com/watch?v=e4EJ34xnlxk */
 
 /*
-Aşağıdakileri cocukSarkisi fonksiyonunda yap:
-1. Başlangıçta var olan maymun sayısını alın.
-2. cocukSarkisi fonksiyonu aşağıdaki satırı sadece 1 kere yazacak şekilde kodunuzu yazın:
+Asagidakileri cocukSarkisi fonksiyonunda yap:
+1. Baslangicta var olan maymun sayisini alin.
+2. cocukSarkisi fonksiyonu asagidaki satiri sadece 1 kere yazacak sekilde kodunuzu yazin:
 
-    "{sayı} küçük maymun yatakta zıplamış, biri düşüp başını çarpmış, Anne doktoru aramış, Doktor çok kızmış: Bir daha yatakta zıplamak yok!"
+    "{sayi} kucuk maymun yatakta ziplamis, biri dusup basini carpmis, Anne doktoru aramis, Doktor cok kizmis: Bir daha yatakta ziplamak yok!"
 
-3. Bu fonksiyonun dışında bir yerde, maymun sayısının her seferinde 1 azaldığı ve maymun sayısı 1 olana kadar devem eden bir döngü oluşturun. 
-4. Bu döngüde, her seferinde cocukSarkisi fonsiyonu çalışsın ve console.log'a dönen metni yazdırsın.
+3. Bu fonksiyonun disinda bir yerde, maymun sayisinin her seferinde 1 azaldigi ve maymun sayisi 1 olana kadar devem eden bir dongu olusturun. 
+4. Bu dongude, her seferinde cocukSarkisi fonsiyonu calissin ve console.log'a donen metni yazdirsin.
 */
 
-function cocukSarkisi(/*buraya kodunu yazabilirsin*/) {
-  /*buraya kodunu yazabilirsin*/
+function cocukSarkisi(num_maymun) {
+  console.log("${num_maymun} kucuk maymun yatakta ziplamis, biri dusup basini carpmis, Anne doktoru aramis, Doktor cok kizmis: Bir daha yatakta ziplamak yok!");
 }
 
-/* Görev 6 : Not Hesaplayıcı */
+for(let i = num_maymun; i > 1; i--)
+{
+  cocukSarkisi(i);
+}
+
+/* Gorev 6 : Not Hesaplayici */
 
 /*
-Aşağdakileri notHesapla fonksiyonunda yap.
-1. 100'lük sistemde bir sınav sonucu alın.
-2. Aşağıdaki tabloya göre notu dönün.
+Asagdakileri notHesapla fonksiyonunda yap.
+1. 100'luk sistemde bir sinav sonucu alin.
+2. Asagidaki tabloya gore notu donun.
 
- 90-100 arasında 'A aldın' 
- 80-89 arasında 'B aldın'
- 70-79 arasında 'C aldın'
- 60-69 arasında 'D aldın'
- daha aşağıda 'F aldın'
- dönün
+ 90-100 arasinda 'A aldin' 
+ 80-89 arasinda 'B aldin'
+ 70-79 arasinda 'C aldin'
+ 60-69 arasinda 'D aldin'
+ daha asagida 'F aldin'
+ donun
 */
 
-function notHesapla(/*buraya kodunu yazabilirsin*/) {
-  /*buraya kodunu yazabilirsin*/
+function notHesapla(sonuc) {
+  if(sonuc >= 90)
+  {
+    console.log("A aldin");
+  }
+  else if(sonuc >= 80)
+  {
+    console.log("B aldin");
+  }
+  else if(sonuc >= 70)
+  {
+    console.log("C aldin");
+  }
+  else if(sonuc >= 60)
+  {
+    console.log("D aldin");
+  }
+  else
+  {
+    console.log("F aldin");
+  }
 }
 
-/* Bonus Çalışma: Sesli harf sayacı - Kaç tane sesli harf var? */
+/* Bonus calisma: Sesli harf sayaci - Kac tane sesli harf var? */
 
 /*
-Aşağıdakileri sesliHarfSayaci fonskiyonunda yap.
-1. Bir argüman alın, string olsun.
-2. Bu string'in içindeki sesli harfleri sayın ve sayısını geri dönün.  (hem büyük hem de küçük harflerin sayısını dönmeli).
+Asagidakileri sesliHarfSayaci fonskiyonunda yap.
+1. Bir arguman alin, string olsun.
+2. Bu string'in icindeki sesli harfleri sayin ve sayisini geri donun.  (hem buyuk hem de kucuk harflerin sayisini donmeli).
 
-İPUCU - yarın işlenecek array(dizi) konusunu önden araştırman gerekecek. (https://www.w3schools.com/js/js_arrays.asp)
+İPUCU - yarin islenecek array(dizi) konusunu onden arastirman gerekecek. (https://www.w3schools.com/js/js_arrays.asp)
 İPUCU - .includes() methoduna bakabilirsin. (https://www.w3schools.com/jsref/jsref_includes.asp)
 */
 
@@ -182,13 +272,13 @@ function sesliHarfSayaci(/*buraya kodunu yazabilirsin*/) {
   /*buraya kodunu yazabilirsin*/
 }
 
-/* Lütfen bu satırın alt tarafını değiştirmeyin */
+/* Lutfen bu satirin alt tarafini degistirmeyin */
 function sa() {
-  console.log("Kodlar çalışıyor");
+  console.log("Kodlar calisiyor");
   return "as";
 }
 sa();
-/* Bu satırdan sonrasını değiştirmeyin */
+/* Bu satirdan sonrasini degistirmeyin */
 module.exports = {
   sa,
   carpma,
